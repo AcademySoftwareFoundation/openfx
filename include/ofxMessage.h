7967e8987b41c6ddc727e9ac361320ac3470a1c4
brunonicoletti <>
2004-05-25 08:44:38 +0000

    Initial import

---
+++ include/ofxMessage.h
@@ -0,0 +1,123 @@
+#ifndef _ofxMessage_h_
+#define _ofxMessage_h_
+
+#include "ofxCore.h"
+
+/*
+Software License :
+
+Copyright (c) 2003-2004, The Foundry Visionmongers Ltd. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name The Foundry Visionmongers Ltd, nor the names of its 
+      contributors may be used to endorse or promote products derived from this
+      software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define kOfxMessageSuite "OfxMessageSuite"
+
+
+/** @brief String used to type fatal error messages
+
+    Fatal error messages should only be posted by a plugin when it can no longer continue operation.
+ */
+#define kOfxMessageFatal "OfxMessageFatal"
+
+/** @brief String used to type error messages 
+
+    Ordinary error messages should be posted when there is an error in operation that is recoverable by
+    user intervention.
+*/
+#define kOfxMessageError "OfxMessageError"
+
+/** @brief String used to type simple ordinary messages 
+
+    Ordinary messages simply convey information from the plugin directly to the user.
+*/
+#define kOfxMessageMessage "OfxMessageMessage"
+
+/** @brief String used to type log messages 
+
+    Log messages are written out to a log and not to the end user.
+*/
+#define kOfxMessageLog "OfxMessageLog"
+
+/** @brief String used to type yes/no messages
+
+    The host is to enter a modal state which waits for the user to respond yes or no. 
+The OfxMessageSuiteV1::message function which posted the message will only return after 
+the user responds. When asking a question, the OfxStatus code returned by the message function will be,
+    - kOfxStatReplyYes - if the user replied 'yes' to the question
+    - kOfxStatReplyNo - if the user replied 'no' to the question
+    - some error code if an error was encounterred
+
+    It is an error to post a question message if the plugin is not in an interactive session.
+ */
+#define kOfxMessageQuestion "OfxMessageQuestion"
+
+/** @brief This struct contains the function pointer for implementing sending simple messages to end users
+ */
+typedef struct OfxMessageSuiteV1 {
+
+  /** @brief Post a message on the host, using printf style varargs
+
+      \arg \e handle     - effect handle (descriptor or instance) the message should be associated with, may be null
+      \arg \e messageType - string describing the kind of message to post, one of the kOfxMessageType* constants
+      \arg \e messageId - plugin specified id to associate with this message. If overriding the message in XML resource, the message is identified with this,
+      \arg \e format    - printf style format string
+      \arg \e ...       - printf style varargs list to print
+
+\returns 
+  - ::kOfxStatOK - if the message was sucessfully posted 
+  - ::kOfxStatReplyYes - if the message was of type  ::kOfxMessageQuestion and the user reply yes
+  - ::kOfxStatReplyNo - if the message was of type kOfxMessageQuestion and the user reply no
+  - ::kOfxStatFailed - if the message could not be posted for some reason
+
+   */
+  OfxStatus (*message)(void *handle,
+		       const char *messageType,
+		       const char *messageId,
+		       const char *format,
+		       ...);
+
+} OfxMessageSuiteV1;
+
+/** @file ofxMessage.h
+    This file contains the Host API for end user message communication.
+*/
+
+/** @page MessagePage OFX : Message Host API
+    
+    see ofxMessage.h
+
+    \em UNFINISHED
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ include/ofxMultiThread.h
@@ -0,0 +1,194 @@
+#ifndef _ofxMultiThread_h_
+#define _ofxMultiThread_h_
+
+/*
+Software License :
+
+Copyright (c) 2003-2004, The Foundry Visionmongers Ltd. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name The Foundry Visionmongers Ltd, nor the names of its 
+      contributors may be used to endorse or promote products derived from this
+      software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#include "ofxCore.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @file ofxMultiThread.h
+
+    This file contains the Host Suite for threading 
+*/
+
+/** @page MultiThreadPage OFX : Multithreading Suite
+    
+    see ofxMultiThread.h
+
+    \em UNFINISHED
+ */
+
+#define kOfxMultiThreadSuite "OfxMultiThreadSuite"
+
+/** @brief Mutex blind data handle
+ */
+typedef struct OfxMutex OfxMutex;
+
+
+/** @brief The function type to passed to the multi threading routines 
+
+    \arg \e threadIndex unique index of this thread, will be between 0 and threadMax
+    \arg \e threadMax to total number of threads executing this function
+    \arg \e customArg the argument passed into multiThread
+
+A function of this type is passed to OfxMultiThreadSuiteV1::multiThread to be launched in multiple threads.
+ */
+typedef void (OfxThreadFunctionV1)(int threadIndex,
+				   unsigned int threadMax,
+				   void *customArg);
+
+/** @brief Host API to carry out SMP multi threaded rendering
+ */
+typedef struct OfxMultiThreadSuiteV1 {
+  /**@brief Function to spawn SMP threads
+
+  \arg \e func The function to call in each thread.
+  \arg \e nThreads The number of threads to launch
+  \arg \e customArg The paramter to pass to customArg of func in each thread.
+
+  This function will spawn nThreads separate threads of computation (typically one per CPU) 
+  to allow something to perform symetric multi processing. Each thread will call 'func' passing
+  in the index of the thread and the number of threads actually launched.
+
+  multiThread will not return until all the spawned threads have returned. It is up to the host
+  how it waits for all the threads to return (busy wait, blocking, whatever).
+
+  \e nThreads can be more than the value returned by multiThreadNumCPUs, however the threads will
+  be limitted to the number of CPUs returned by multiThreadNumCPUs.
+
+  This function cannot be called recursively.
+
+  \returns
+  - ::kOfxStatOK, the function func has executed and returned sucessfully
+  - ::kOfxStatFailed, the threading function failed to launch
+
+  */
+  OfxStatus (*multiThread)(OfxThreadFunctionV1 func,
+			   unsigned int nThreads,
+			   void *customArg);
+			  
+  /**@brief Function which indicates the number of CPUs available for SMP processing
+
+  \arg \e nCPUs pointer to an integer where the result is returned
+     
+  This value may be less than the actual number of CPUs on a machine, as the host may reserve other CPUs for itself.
+
+  \returns
+  - ::kOfxStatOK, all was OK and the maximum number of threads is in nThreads.
+  - ::kOfxStatFailed, the function failed to get the number of CPUs 
+  */
+  OfxStatus (*multiThreadNumCPUs)(unsigned int *nCPUs);
+
+  /**@brief Function which indicates the index of the current thread
+
+  \arg \e threadIndex  pointer to an integer where the result is returned
+
+  This function returns the thread index, which is the same as the \e threadIndex argument passed to the ::OfxThreadFunctionV1.
+
+  If there are no threads currently spawned, then this function will set threadIndex to 0
+
+  \returns
+  - ::kOfxStatOK, all was OK and the maximum number of threads is in nThreads.
+  - ::kOfxStatFailed, the function failed to return an index
+  */
+  OfxStatus (*multiThreadIndex)(unsigned int *threadIndex);
+
+  /**@brief Function to enquire if the calling thread was spawned by multiThread
+
+  \returns
+  - 0 if the thread is not one spawned by multiThread
+  - 1 if the thread was spawned by multiThread
+  */
+  int (*multiThreadIsSpawnedThread)(void);
+
+  /** @brief Create a mutex
+
+  \arg \e mutex - where the new handle is returned
+  \arg \e count - initial lock count on the mutex. This can be negative.
+
+  Creates a new mutex with lockCount locks on the mutex intially set.    
+
+  \returns
+  - kOfxStatOK - mutex is now valid and ready to go
+  */
+  OfxStatus (*mutexCreate)(const OfxMutex **mutex, int lockCount);
+
+  /** @brief Destroy a mutex
+      
+  Destroys a mutex intially created by mutexCreate.
+  
+  \returns
+  - kOfxStatOK - if it destroyed the mutex
+  - kOfxStatErrBadHandle - if the handle was bad
+  */
+  OfxStatus (*mutexDestroy)(const OfxMutex *mutex);
+
+  /** @brief Blocking lock on the mutex
+
+  This trys to lock a mutex and blocks the thread it is in until the lock suceeds.
+  
+  \returns
+  - kOfxStatOK - if it got the lock
+  - kOfxStatErrBadHandle - if the handle was bad
+  */
+  OfxStatus (*mutexLock)(const OfxMutex *mutex);
+
+  /** @brief Unlock the mutex
+
+  This  unlocks a mutex.
+  
+  \returns
+  - kOfxStatOK if it released the lock
+  - kOfxStatErrBadHandle if the handle was bad
+  */
+  OfxStatus (*mutexUnLock)(const OfxMutex *mutex);
+
+  /** @brief Non blocking attempt to lock the mutex
+
+  This attempts to lock a mutex, if it cannot, it returns and says so, rather than blocking.
+
+  \returns
+  - kOfxStatOK - if it got the lock
+  - kOfxStatFailed - if it did not get the lock
+  - kOfxStatErrBadHandle - if the handle was bad
+  */
+  OfxStatus (*mutexTryLock)(const OfxMutex *mutex);
+
+ } OfxMultiThreadSuiteV1;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
