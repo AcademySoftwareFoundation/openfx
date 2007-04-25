74bcface17280be8b5b1315a0cff712e7a647ce0
andywhitmore <>
2007-04-25 12:58:37 +0000

    exercising some actions

--- HostSupport/examples/hostDemo.cpp
+++ HostSupport/examples/hostDemo.cpp
@@ -69,5 +69,29 @@ int main(int argc, char **argv)
   // create an instance
   OFX::Host::ImageEffect::Instance* tileFilterInstance = plugin->createInstance(kOfxImageEffectContextFilter);
 
+  // begin instance
+  tileFilterInstance->beginInstanceChangedAction(kOfxChangeUserEdited);
+
+  // instance change
+  tileFilterInstance->paramInstanceChangedAction("softness",kOfxChangeUserEdited,0.0,1.0,1.0);
+
+  // end instance
+  tileFilterInstance->endInstanceChangedAction(kOfxChangeUserEdited);
+
+  // purge your caches
+  tileFilterInstance->purgeCachesAction();
+
+  // sync your private data
+  tileFilterInstance->syncPrivateDataAction();
+
+  // begin instance
+  tileFilterInstance->beginInstanceEditAction();
+
+  // end instance
+  tileFilterInstance->endInstanceEditAction();
+
+  // delete the instance
+  delete tileFilterInstance;
+
   return 0;
 }
