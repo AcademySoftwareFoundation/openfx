3bfcaeaa7f3f96f813a25c548151ac12e8cd24b6
brunonicoletti <>
2007-06-18 14:13:16 +0000

    New progress suite

---
+++ include/ofxProgress.h
@@ -0,0 +1,60 @@
+#ifndef _ofxProgressSuite_h_
+#define _ofxProgressSuite_h_
+
+
+#define kOfxProgressSuite "OfxProgressSuite"
+
+/** @brief A suite that provides progress feedback from a plugin to an application */
+typedef struct OfxProgressSuiteV1 {  
+
+  /** @brief Initiate a progress bar display.
+      
+      Call this to initiate the display of a progress bar.
+      
+      \arg \e effectInstance - the instance of the plugin this progress bar is
+                               associated with. It cannot be NULL.
+      \arg \e label          - a text label to display in any message portion of the
+                               progress object's user interface. A UTF8 string.
+
+      \returns
+      - ::kOfxStatOK - the handle is now valid for use
+      - ::kOfxStatFailed - the progress object failed for some reason
+      - ::kOfxStatErrBadHandle - effectInstance was invalid
+   */
+  OfxStatus (*progressStart)(void *effectInstance,
+                             const char *label);
+
+  /** @brief Indicate how much of the processing task has been completed and reports on any abort status.
+      
+      \arg \e effectInstance - the instance of the plugin this progress bar is
+                                associated with. It cannot be NULL.
+      \arg \e progress - a number between 0.0 and 1.0 indicating what proportion of the current task has been processed.
+      
+      \returns
+      - ::kOfxStatOK - the progress object was successfully updated and the task should continue
+      - ::kOfxStatReplyNo - the progress object was successfully updated and the task should abort
+      - ::kOfxStatErrBadHandle - the progress handle was invalid,
+  */
+  OfxStatus (*progressUpdate)(void *effectInstance, double progress);
+
+  /** @brief Signal that we are finished with the progress meter.
+      
+      Call this when you are done with the progress meter and no
+      longer need it displayed.
+
+      \arg \e effectInstance - the instance of the plugin this progress bar is
+                                associated with. It cannot be NULL.
+      
+      \postcondition - you can no longer call progressUpdate on the instance                       
+
+      \returns
+      - ::kOfxStatOK - the progress object was successfully closed
+      - ::kOfxStatErrBadHandle - the progress handle was invalid,
+   */
+  OfxStatus (*progressEnd)(void *effectInstance);
+
+
+
+} OfxProgressSuiteV1 ;
+
+#endif
---
+++ include/ofxTimeLine.h
@@ -0,0 +1,58 @@
+#ifndef _ofxTimeLine_h_
+#define _ofxTimeLine_h_
+
+/** @brief Name of the time line suite */
+#define kOfxTimeLineSuite "OfxTimeLineSuite"
+
+/** @brief Suite to control timelines */
+typedef struct OfxTimeLineSuiteV1 {
+  /** @brief Get the time value of the timeline that is controlling to the indicated effect.
+      
+      \arg instance is the instance of the effect changing the timeline, cast to a void *
+      \arg time a pointer through which the timeline value should be returned
+       
+      This function moves the timeline to the indicated frame and returns. Any side effects of the timeline
+      change are also triggered and completed before this returns (for example instance changed actions and renders
+      if the output of the effect is being viewed).
+
+      @returns
+      - ::kOfxStatOK - the time enquiry was sucessful
+      - ::kOfxStatFailed - the enquiry failed for some host specific reason
+      - ::kOfxStatErrBadHandle - the effect handle was invalid
+  */
+  OfxStatus (*getTime)(void *instance, double *time);
+
+  /** @brief Move the timeline control to the indicated time.
+      
+      \arg instance is the instance of the effect changing the timeline, cast to a void *
+      \arg time is the time to change the timeline to. This is in the temporal coordinate system of the effect.
+       
+      This function moves the timeline to the indicated frame and returns. Any side effects of the timeline
+      change are also triggered and completed before this returns (for example instance changed actions and renders
+      if the output of the effect is being viewed).
+
+      @returns
+      - ::kOfxStatOK - the time was changed sucessfully, will all side effects if the change completed
+      - ::kOfxStatFailed - the change failed for some host specific reason
+      - ::kOfxStatErrBadHandle - the effect handle was invalid
+      - ::kOfxStatErrValue - the time was an illegal value       
+  */
+  OfxStatus (*gotoTime)(void *instance, double time);
+
+  /** @brief Get the current bounds on a timeline
+      
+      \arg instance is the instance of the effect changing the timeline, cast to a void *
+      \arg firstTime is the first time on the timeline. This is in the temporal coordinate system of the effect.
+      \arg lastTime is last time on the timeline. This is in the temporal coordinate system of the effect.
+       
+      This function
+
+      @returns
+      - ::kOfxStatOK - the time enquiry was sucessful
+      - ::kOfxStatFailed - the enquiry failed for some host specific reason
+      - ::kOfxStatErrBadHandle - the effect handle was invalid
+  */
+  OfxStatus (*getTimeBounds)(void *instance, double *firstTime, double *lastTime);
+} OfxTimeLineSuiteV1;
+
+#endif
