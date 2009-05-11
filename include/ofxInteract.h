f83b3e8cd890b888a140d630c6da2a03850a2501
brunonicoletti <>
2009-05-11 15:53:07 +0000

    Changed the copyright notice to reflect the transfer of the IP to The Foundry

--- include/ofxInteract.h
+++ include/ofxInteract.h
@@ -6,7 +6,7 @@
 /*
 Software License :
 
-Copyright (c) 2003-2004, The Foundry Visionmongers Ltd. All rights reserved.
+Copyright (c) 2007-2009, The Open Effects Association Ltd. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -16,7 +16,7 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright notice,
       this list of conditions and the following disclaimer in the documentation
       and/or other materials provided with the distribution.
-    * Neither the name The Foundry Visionmongers Ltd, nor the names of its 
+    * Neither the name The Open Effects Association Ltd, nor the names of its 
       contributors may be used to endorse or promote products derived from this
       software without specific prior written permission.
 
