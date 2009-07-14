bd217c800279261a90ded776f2926c79e3e7610c
brunonicoletti <>
2009-07-14 14:38:28 +0000

    Changed the copyright notification to reflect chnage in ownership

--- HostSupport/include/ofxhPropertySuite.h
+++ HostSupport/include/ofxhPropertySuite.h
@@ -2,7 +2,7 @@
 /*
 Software License :
 
-Copyright (c) 2007, The Foundry Visionmongers Ltd. All rights reserved.
+Copyright (c) 2007, The Open Effects Association Ltd. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -12,7 +12,7 @@ this list of conditions and the following disclaimer.
 * Redistributions in binary form must reproduce the above copyright notice,
 this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
-* Neither the name The Foundry Visionmongers Ltd, nor the names of its 
+* Neither the name The Open Effects Association Ltd, nor the names of its 
 contributors may be used to endorse or promote products derived from this
 software without specific prior written permission.
 
@@ -34,7 +34,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /*
 Software License :
 
-Copyright (c) 2007, The Foundry Visionmongers Ltd. All rights reserved.
+Copyright (c) 2007, The Open Effects Association Ltd. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -44,7 +44,7 @@ this list of conditions and the following disclaimer.
 * Redistributions in binary form must reproduce the above copyright notice,
 this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
-* Neither the name The Foundry Visionmongers Ltd, nor the names of its 
+* Neither the name The Open Effects Association Ltd, nor the names of its 
 contributors may be used to endorse or promote products derived from this
 software without specific prior written permission.
 
--- HostSupport/src/ofxhPluginCache.cpp
+++ HostSupport/src/ofxhPluginCache.cpp
@@ -1,7 +1,7 @@
 /*
 Software License :
 
-Copyright (c) 2007, The Foundry Visionmongers Ltd. All rights reserved.
+Copyright (c) 2007, The Open Effects Association Ltd. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -11,7 +11,7 @@ this list of conditions and the following disclaimer.
 * Redistributions in binary form must reproduce the above copyright notice,
 this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
-* Neither the name The Foundry Visionmongers Ltd, nor the names of its 
+* Neither the name The Open Effects Association Ltd, nor the names of its 
 contributors may be used to endorse or promote products derived from this
 software without specific prior written permission.
 
