3f67ba3bba45127e5dd0e6ca35eb586a00e8ac6a
Frédéric Devernay <frederic.devernay@m4x.org>
2013-12-17 12:25:31 +0100

    Revert "Hide implementation details into ofxhParam.cpp so the host doesn’t have to be aware of the existence of the static member"
    This reverts commit 8fb58d81579debb7fbeed23e417cc5e617860fb1.

--- HostSupport/src/ofxhImageEffect.cpp
+++ HostSupport/src/ofxhImageEffect.cpp
@@ -2487,7 +2487,7 @@ namespace OFX {
         
       void Host::registerExtraParamTypeSupported(const std::string& typeName,Property::TypeEnum type,int dimension)
       {
-          Param::addStandardType(typeName, type, dimension);
+          Param::globalParamsTypeRegistery.addStandardType(typeName, type, dimension);
       }
 
     } // ImageEffect
