e7d52f619c6723f1cb01eca572d596173135f35a
Frédéric Devernay <frederic.devernay@m4x.org>
2014-02-21 14:20:41 +0100

    ParamSetDescriptor::defineParametricParam: don’t crash in case of error

--- Support/Library/ofxsParams.cpp
+++ Support/Library/ofxsParams.cpp
@@ -1180,9 +1180,10 @@ namespace OFX {
   ParametricParamDescriptor* ParamSetDescriptor::defineParametricParam(const std::string &name)
   {
     ParametricParamDescriptor* param = NULL;
-    defineParamDescriptor(name, eParametricParam, param);
-    // Parametric parameters need the ParamSet !
-    param->setParamSet(*this);
+    if (defineParamDescriptor(name, eParametricParam, param)) {
+      // Parametric parameters need the ParamSet !
+      param->setParamSet(*this);
+    }
     return param;
   }
 
