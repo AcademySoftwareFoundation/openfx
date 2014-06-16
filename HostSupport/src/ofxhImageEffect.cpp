35ed54e53070aa61e66caa4fc4a426f205e55141
MrKepzie <alexandre230@msn.com>
2014-06-16 11:00:14 +0200

    Move  kOfxImageEffectInstancePropSequentialRender on the effect descriptor properties instead, so the plug-in can set this in the describe action.

--- HostSupport/src/ofxhImageEffect.cpp
+++ HostSupport/src/ofxhImageEffect.cpp
@@ -90,6 +90,7 @@ namespace OFX {
         { kOfxImageEffectPropClipPreferencesSlaveParam, Property::eString, 0, false, "" },
         { kOfxPluginPropFilePath, Property::eString, 1, true, ""},
         { kOfxPropPluginDescription, Property::eString, 1,false, ""},
+        { kOfxImageEffectInstancePropSequentialRender, Property::eInt, 1, false , "0" },
 #ifdef OFX_EXTENSIONS_NUKE
         { kFnOfxImageEffectPropMultiPlanar,   Property::eInt, 1, false, "0" },
         { kFnOfxImageEffectPropViewAware,   Property::eInt, 1, false, "0" },
@@ -348,7 +349,6 @@ namespace OFX {
         { kOfxImageEffectPropProjectExtent,     Property::eDouble,     2, true,  "0" },
         { kOfxImageEffectPropProjectPixelAspectRatio, Property::eDouble, 1, true,  "0" },
         { kOfxImageEffectInstancePropEffectDuration, Property::eDouble, 1, true,  "0" },
-        { kOfxImageEffectInstancePropSequentialRender, Property::eInt, 1, false, "0" },
         { kOfxImageEffectPropFrameRate ,        Property::eDouble,     1, true,  "0" },
         { kOfxPropIsInteractive,                Property::eInt,        1, true, "0" },
 #ifdef OFX_EXTENSIONS_NUKE
