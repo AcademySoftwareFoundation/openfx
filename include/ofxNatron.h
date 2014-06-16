7deee17cdb03af69a9e7ed545d97fd35096b1aab
Frédéric Devernay <frederic.devernay@m4x.org>
2014-06-16 16:55:47 +0200

    ofxNatron.h: better doc

--- include/ofxNatron.h
+++ include/ofxNatron.h
@@ -4,7 +4,16 @@
 /** @brief The kOfxPropName property of the Natron Host */
  #define kOfxNatronHostName          "fr.inria.Natron"
 
-/** @brief The name of a string parameter (which should be secret) that maps to a sublabel in the GUI, e.g. the name of the file in a Reader node, or the name of the operation in a Merge node. */
+/** @brief The name of a string parameter that maps to a sublabel in the GUI,
+    e.g. the name of the file in a Reader node, or the name of the operation
+    in a Merge node.
+    This parameter should have the properties
+    - kOfxParamPropSecret=1,
+    - kOfxParamPropEnabled=0,
+    - kOfxParamPropPersistant=1,
+    - kOfxParamPropEvaluateOnChange=0.
+    You should set its default value during the kOfxImageEffectActionDescribeInContext action.
+ */
 #define kOfxParamStringSublabelName "OfxParamStringSublabelName"
 
 #endif // #ifndef _ofxNatron_h_
