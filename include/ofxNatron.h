d398f9c98db134fc5a1294743dbdcb7d708fa6ff
Frédéric Devernay <frederic.devernay@m4x.org>
2014-06-16 15:53:23 +0200

    add ofxNatron.h for Natron extensions

---
+++ include/ofxNatron.h
@@ -0,0 +1,10 @@
+#ifndef _ofxNatron_h_
+#define _ofxNatron_h_
+
+/** @brief The kOfxPropName property of the Natron Host */
+ #define kOfxNatronHostName          "fr.inria.Natron"
+
+/** @brief The name of a string parameter (which should be secret) that maps to a sublabel in the GUI, e.g. the name of the file in a Reader node, or the name of the operation in a Merge node. */
+#define kOfxParamStringSublabelName "OfxParamStringSublabelName"
+
+#endif // #ifndef _ofxNatron_h_
