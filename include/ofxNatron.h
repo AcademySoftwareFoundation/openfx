a0cca55c3cbc4eadf4a565e8abae8c0cf469c651
Frédéric Devernay <frederic.devernay@m4x.org>
2014-06-24 14:19:54 +0200

    ofxNatron: update comment.

--- include/ofxNatron.h
+++ include/ofxNatron.h
@@ -2,14 +2,19 @@
 #define _ofxNatron_h_
 
 /** @brief The kOfxPropName property of the Natron Host */
- #define kOfxNatronHostName          "fr.inria.Natron"
+#define kOfxNatronHostName          "fr.inria.Natron"
 
 /** @brief The name of a string parameter that maps to a sublabel in the GUI,
     e.g. the name of the file in a Reader node, or the name of the operation
-    in a Merge node.
+    in a Merge node, or the name of the track in a Tracker node.
+
+    Whenever the host sets the value of this parameter, 
+    kOfxActionInstanceChanged is called (as with any other parameter).
+    It may be kOfxParamPropSecret=1 if it should not appear in the instance UI.
+    It may be kOfxParamPropEnabled=0 if only the host should set its value via
+    a special GUI.
+
     This parameter should have the properties
-    - kOfxParamPropSecret=1,
-    - kOfxParamPropEnabled=0,
     - kOfxParamPropPersistant=1,
     - kOfxParamPropEvaluateOnChange=0.
     You should set its default value during the kOfxImageEffectActionDescribeInContext action.
