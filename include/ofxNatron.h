635c3ad524da3605fac2f99a200d185c0c7f79a5
Frédéric Devernay <frederic.devernay@m4x.org>
2015-03-07 21:34:17 +0100

    ofxNatron: one more

--- include/ofxNatron.h
+++ include/ofxNatron.h
@@ -2,7 +2,7 @@
 #define _ofxNatron_h_
 
 /** @brief The kOfxPropName property of the Natron Host */
-#define kOfxNatronHostName          "fr.inria.Natron"
+#define kNatronOfxHostName          "fr.inria.Natron"
 
 /** @brief The name of a string parameter that maps to a sublabel in the GUI,
     e.g. the name of the file in a Reader node, or the name of the operation
