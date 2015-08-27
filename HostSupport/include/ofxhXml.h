64ba52fff61894759fbf3942b92659b02b2b17cd
Frédéric Devernay <frederic.devernay@m4x.org>
2015-08-27 12:04:08 +0200

    HostSupport: fix XML in the plugin cache
    - see http://www.w3.org/TR/xml/#syntax and
    http://www.w3.org/TR/xml/#AVNormalize

--- HostSupport/include/ofxhXml.h
+++ HostSupport/include/ofxhXml.h
@@ -37,10 +37,44 @@ namespace OFX {
     inline std::string escape(const std::string &s) {
       std::string ns;
       for (size_t i=0;i<s.size();i++) {
-        if (s[i] == '\"') {
-          ns += "&quot;";
-        } else {
-          ns += s[i];
+        // The are exactly five characters which must be escaped
+        // http://www.w3.org/TR/xml/#syntax
+        switch (s[i]) {
+          case '<':
+            ns += "&lt;";
+            break;
+          case '>':
+            ns += "&gt;";
+            break;
+          case '&':
+            ns += "&amp;";
+            break;
+          case '"':
+            ns += "&quot;";
+            break;
+          case '\'':
+            ns += "&apos;";
+            break;
+          default: {
+            unsigned char c = (unsigned char)(s[i]);
+            // Escape even the whitespace characters '\n' '\r' '\t', although they are valid
+            // XML, because they would be converted to space when re-read.
+            // See http://www.w3.org/TR/xml/#AVNormalize
+            if ((0x01 <= c && c <= 0x1f) || (0x7F <= c && c <= 0x9F)) {
+              // these characters must be escaped in XML 1.1
+              // http://www.w3.org/TR/xml/#sec-references
+              ns += "&#x";
+              if (c > 0xf) {
+                int d = c / 0x10;
+                ns += d < 10 ? ('0' + d) : ('A' + d - 10);
+              }
+              int d = c & 0xf;
+              ns += d < 10 ? ('0' + d) : ('A' + d - 10);
+              ns += ';';
+            } else {
+              ns += s[i];
+            }
+          } break;
         }
       }
       return ns;
