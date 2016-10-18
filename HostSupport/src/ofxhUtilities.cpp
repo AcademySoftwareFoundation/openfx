f374e79d3ac295152e197d1c0cc6841e42ea5b09
MrKepzie <alexandre230@msn.com>
2016-10-18 16:59:34 +0200

    utf8_to_utf16: bug fix

--- HostSupport/src/ofxhUtilities.cpp
+++ HostSupport/src/ofxhUtilities.cpp
@@ -70,7 +70,7 @@ namespace OFX {
     std::wstring native;
     
     
-    native.resize(MultiByteToWideChar (CP_UTF8, 0, s.c_str(), -1, NULL, 0) -1);
+    native.resize(MultiByteToWideChar (CP_UTF8, 0, s.c_str(), -1, NULL, 0));
     MultiByteToWideChar (CP_UTF8, 0, s.c_str(), s.size(), &native[0], (int)native.size());
     
     return native;
