f2cd9fdf53f6986c013c6d4e18e4b97d3f965ac4
Alexandre Gauthier <alexandre230@msn.com>
2018-01-16 10:14:45 +0100

    Revert "Experiment theme style change for the documentation"
    This reverts commit 48ee5400eba754c2147710227c0c4dc7ec14428c.

--- Documentation/sources/conf.py
+++ Documentation/sources/conf.py
@@ -95,16 +95,13 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "nosidebar": "true",
-    "footertextcolor": "#000000"
-}
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
