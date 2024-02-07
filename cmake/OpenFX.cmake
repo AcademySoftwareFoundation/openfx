b3f99ed183b9e75fafdfee6d680b058194f6b787
John-Paul Smith <jps@borisfx.com>
2024-02-07 21:29:45 +0000

    Remove symbol files as these are no longer needed
    Both the CMake and Makefile projects included compiler arguments to set symbol visibility to hidden and expose only the OfxGetNumberOfPlugins and OfxGetPlugin symbols. The CMake project has been changed to use built-in functionality to control symbol visibility, reducing the amount of platform-specific code that needs to be maintained.
    
    Furthermore, all our examples use __attribute__((visibility("default"))) to expose necessary symbols, rendering the symbol files unnecessary, so all references to the symbol files, and the files themselves have been removed.
    
    Signed-off-by: John-Paul Smith <jps@borisfx.com>

--- cmake/OpenFX.cmake
+++ cmake/OpenFX.cmake
@@ -28,31 +28,13 @@ function(add_ofx_plugin TARGET)
 	endif()
 	set_target_properties(${TARGET} PROPERTIES SUFFIX ".ofx" PREFIX "")
 
-	if(NOT DEFINED OFX_SUPPORT_SYMBOLS_DIR)
-		if (NOT DEFINED CONAN_OPENFX_ROOT)
-			message(FATAL_ERROR "Define OFX_SUPPORT_SYMBOLS_DIR to use add_ofx_plugin().")
-		endif()
-		set(OFX_SUPPORT_SYMBOLS_DIR ${CONAN_OPENFX_ROOT}/symbols)
-	endif()
+   # Set symbol visibility hidden. Individual symbols are exposed via
+   # __declspec(dllexport) or __attribute__((visibility("default")))
+   set_target_properties(${TARGET} PROPERTIES
+      C_VISIBILITY_PRESET hidden
+      CXX_VISIBILITY_PRESET hidden)
 
-	# Add extra flags to the link step of the plugin
-	if(APPLE)
-		set_target_properties(${TARGET} PROPERTIES
-                  LINK_FLAGS "-fvisibility=hidden -exported_symbols_list,${OFX_SUPPORT_SYMBOLS_DIR}/osx.symbols")
-	elseif(WIN32)
-		if (MSVC)
-			set_target_properties(${TARGET} PROPERTIES
-                    LINK_FLAGS "/def:${OFX_SUPPORT_SYMBOLS_DIR}/windows.symbols")
-    elseif(MINGW OR MSYS)
-      set_target_properties(${TARGET} PROPERTIES
-                    LINK_FLAGS "-Wl,-fvisibility=hidden,--version-script=${OFX_SUPPORT_SYMBOLS_DIR}/mingw.symbols")
-    endif()
-	else()
-		set_target_properties(${TARGET} PROPERTIES
-                  LINK_FLAGS "-Wl,-fvisibility=hidden,--version-script=${OFX_SUPPORT_SYMBOLS_DIR}/linux.symbols")
-	endif()
-
-        # To install plugins: cmake --install Build
-        install(TARGETS ${TARGET} DESTINATION "${PLUGINDIR}/${TARGET}.ofx.bundle/Contents/${ARCHDIR}")
-        install(FILES ${DIR}/Info.plist DESTINATION "${PLUGINDIR}/${TARGET}.ofx.bundle/Contents")
+   # To install plugins: cmake --install Build
+   install(TARGETS ${TARGET} DESTINATION "${PLUGINDIR}/${TARGET}.ofx.bundle/Contents/${ARCHDIR}")
+   install(FILES ${DIR}/Info.plist DESTINATION "${PLUGINDIR}/${TARGET}.ofx.bundle/Contents")
 endfunction()
